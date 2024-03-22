903876d8923f23a0284d8ed3781b3202e719e117
pkhubpp8 <15952869+pkhubpp8@users.noreply.github.com>
2024-03-23 01:53:26 +0800

    mt架构调整，title校验修改

--- target/mteamSign.py
+++ target/mteamSign.py
@@ -7,7 +7,7 @@ from ._BASE import signBase
 logger = logging.getLogger('sign')
 
 class signClass(signBase):
-    def __init__(self, driver, url = 'https://kp.m-team.cc/index.php', module_name: str = 'mteamSign'):
+    def __init__(self, driver, url = 'https://kp.m-team.cc', module_name: str = 'mteamSign'):
         self.indexUrl = url
         self.driver = driver
         self.module_name = module_name
@@ -16,11 +16,11 @@ class signClass(signBase):
         self.driver.execute_script("window.open('', '_blank');")  # 打开新标签页
         self.driver.switch_to.window(self.driver.window_handles[-1])  # 切换到新标签页
         self.driver.get(self.indexUrl)  # 打开链接
-        time.sleep(2)
+        time.sleep(10)
     def sign(self):
         pass
     def validSign(self):
-        if not re.search('M-Team.*NexusPHP', self.driver.title):
+        if not re.search('M-Team.*Powered by', self.driver.title):
             self.sign_result = False
             self.sign_result_info = f"标题异常：{self.driver.title}"
             return False
