7466b89eb1b34c89444da1dea99edd7552f59f9c
pkhubpp8 <15952869+pkhubpp8@users.noreply.github.com>
2024-06-10 12:46:36 +0800

    新增

---
+++ target/kylinSign.py
@@ -0,0 +1,68 @@
+from selenium.webdriver.common.by import By
+import re
+import logging
+import time
+from ._BASE import signBase
+
+logger = logging.getLogger('sign')
+
+class signClass(signBase):
+    def __init__(self, url = 'https://na.hdkylin.com/index.php', module_name: str = 'kylinSign'):
+        self.indexUrl = url
+        self.module_name = module_name
+        super().__init__("kylin")
+    def accessIndex(self):
+        self.driver.get(self.indexUrl)  # 打开链接
+        time.sleep(10)
+    def msgCheck(self) -> bool:
+        elements = self.driver.find_elements(By.PARTIAL_LINK_TEXT, "条新短讯！点击查看")
+        if len(elements) == 1:
+            self.new_message = elements[0].text.strip()
+            return True
+        elif len(elements) == 0:
+            return False
+        else:
+            self.new_message = "warning: " + elements[0].text.strip()
+            logger.warning(f"找到elements长度{len(elements)}异常")
+            return True
+    def sign(self):
+        elements = self.driver.find_elements(By.CLASS_NAME, "faqlink")
+        for element in elements:
+            if element.text == '[签到得魔力]' and 'attendance.php' in element.get_attribute("href"):
+                element.click()
+                break
+    def validSign(self):
+        if not re.search('HDKylin.*NexusPHP', self.driver.title):
+            self.sign_result = False
+            self.sign_result_info = f"标题异常：{self.driver.title}"
+            return False
+        elements = self.driver.find_elements(By.CLASS_NAME, "text")
+        for element in elements:
+            match = re.search(r'这是[你您]的第\s+(\d+)\s+次签到.*已连续签到\s+(\d+)\s+天.*本次签到获得\s+(\d+)\s+个魔力值', element.text)
+            if match:
+                self.sign_result = True
+                self.sign_result_info = f"第{match.group(1)}次签到，连续签到{match.group(2)}，获得魔力{match.group(3)}"
+                return True
+        elements = self.driver.find_elements(By.CSS_SELECTOR, 'a[class=""]')
+        for element in elements:
+            match = re.search(r'\[签到已得(\d+), 补签卡:\s*\d+\]', element.text)
+            if match:
+                self.sign_result = True
+                self.sign_result_info = f"已经签到过了。签到已得{match.group(1)}"
+                return True
+        self.sign_result = False
+        self.sign_result_info = f"未知异常。"
+        return False
+    def collect_info(self) -> dict:
+        self.result = {
+            "module_name": self.module_name,
+            "site_name": self.site_name,
+            "site_url": self.indexUrl,
+            "sign_result": self.sign_result,
+            "sign_result_info": self.sign_result_info,
+            "date_and_time": int(time.time()),
+            "need_resign": self.need_resign,
+            "new_message": self.new_message,
+            "extra_info": self.extra_info
+        }
+        return self.result
